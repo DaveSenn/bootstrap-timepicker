a983a1edf348f9d6ffb1307acb12c84e9d98315b
Dave Senn <dave.jon.senn@gmail.com>
2017-03-23 09:13:03 +0100

    Fix input when hour >= maxHours

--- js/bootstrap-timepicker.js
+++ js/bootstrap-timepicker.js
@@ -852,7 +852,7 @@
 
         minute = timeArray[1] ? timeArray[1].toString() : '';
         second = timeArray[2] ? timeArray[2].toString() : '';
-
+		console.log('hi 1');
         // adaptive time parsing
         if (hour.length > 4) {
           second = hour.slice(-2);
@@ -897,6 +897,11 @@
           // No day/date handling.
           hour = this.maxHours - 1;
         }
+		// Change
+		if(hour >= this.maxHours - 1) {
+			minute = 0;
+			console.log('reset minute')
+		}
 
         if (this.showMeridian) {
           if (hour > 12) {
@@ -1054,9 +1059,10 @@
       var t = this.$widget.find('input.bootstrap-timepicker-hour').val() + ':' +
               this.$widget.find('input.bootstrap-timepicker-minute').val() +
               (this.showSeconds ? ':' + this.$widget.find('input.bootstrap-timepicker-second').val() : '') +
-              (this.showMeridian ? this.$widget.find('input.bootstrap-timepicker-meridian').val() : '')
-      ;
+              (this.showMeridian ? this.$widget.find('input.bootstrap-timepicker-meridian').val() : '');
 
+		console.log('UPDATE FROM INPUT');
+			  
       this.setTime(t, true);
     },
 
